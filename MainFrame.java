9af56830ef45029cfeb63c0b654c88bf89734e73
Cram974 <cram.dijoux@gmail.com>
2014-08-28 09:45:49 +0400

    Added main application window

---
+++ MainFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    
+    public MainFrame(String title, int height, int width){
+        this.setTitle(title);
+        this.setSize(width, height);
+    }
+}
---
+++ Paint.java
@@ -0,0 +1,6 @@
+public class Paint {
+    public static void main(String[] args){
+        MainFrame window = new MainFrame("Paint", 200, 300);
+        window.setVisible(true);
+    }
+}
